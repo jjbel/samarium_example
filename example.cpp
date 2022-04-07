f01491a4ea067887238017a879e5a97ad2018c55
Jai Bellare <jaibellare@gmail.com>
2022-04-07 18:47:52 +0530

    run clang format

--- example.cpp
+++ example.cpp
@@ -5,9 +5,10 @@
 
 #include "samarium/samarium.hpp"
 
-int main() {
-  fmt::print(fmt::emphasis::bold, "\nSuccessful installation!\n");
-  fmt::print(fmt::emphasis::bold, "Welcome to {}\n", sm::version);
-  sm::print("A Vector2:", sm::Vector2{.x = 5, .y = -3});
-  sm::print("A Color:  ", sm::Color{.r = 5, .g = 200, .b = 255});
+int main()
+{
+    fmt::print(fmt::emphasis::bold, "\nSuccessful installation!\n");
+    fmt::print(fmt::emphasis::bold, "Welcome to {}\n", sm::version);
+    sm::print("A Vector2:", sm::Vector2{.x = 5, .y = -3});
+    sm::print("A Color:  ", sm::Color{.r = 5, .g = 200, .b = 255});
 }
